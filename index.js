533bc77b85716446aefeea675a418e098f5c7267
ashonasho <ashonmcmillan1234@gmail.com>
2023-12-22 19:34:22 +0530

    changed mouse over to mouse move

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 
-
 const initCanvas = (id) => {
     return new fabric.Canvas(id, {
         width: 500, 
@@ -16,11 +15,27 @@ const setBackground = (url, canvas) => {
 }
 
 const canvas = initCanvas("canvas");
+let mousePressed = false;
 
 setBackground('https://i.pinimg.com/564x/aa/85/fe/aa85fedb590860fdeff1731fbb52ddb0.jpg', canvas);
 
 // mouse:over
-canvas.on('mouse:over', (e) => {
-    console.log(e)
+canvas.on('mouse:move', (event) => {
+    // console.log(event)
+    if (mousePressed) {
+        const mEvent = event.e;
+        const delta = new fabric.Point(mEvent.movementX,mEvent.movementY)
+        canvas.relativePan(delta)
+    } 
+}) 
 
+// mouse:down
+canvas.on("mouse:down", (event) => {
+    mousePressed = true;
 })
+
+// mouse:up
+canvas.on("mouse:up", (event) => {
+    mousePressed = false;
+})
+
