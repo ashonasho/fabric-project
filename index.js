ffdf0b6f55a718e3bdf417fbd250758f53f424a9
ashonasho <ashonmcmillan1234@gmail.com>
2023-12-22 14:42:44 +0530

    mouse over

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 
 
-console.log("I am in index.js")
-const canvas = new fabric.Canvas("canvas",{
-    width: 500,
-    height: 500
-});
-canvas.renderAll();
+const initCanvas = (id) => {
+    return new fabric.Canvas(id, {
+        width: 500, 
+        height: 500,
+    });
+}
 
-fabric.Image.fromURL("https://i.pinimg.com/564x/aa/85/fe/aa85fedb590860fdeff1731fbb52ddb0.jpg",(img) => {
+const setBackground = (url, canvas) => {
+    fabric.Image.fromURL(url, (img) => {
     canvas.backgroundImage = img
     canvas.renderAll()
 }) 
+
+}
+
+const canvas = initCanvas("canvas");
+
+setBackground('https://i.pinimg.com/564x/aa/85/fe/aa85fedb590860fdeff1731fbb52ddb0.jpg', canvas);
+
+// mouse:over
+canvas.on('mouse:over', (e) => {
+    console.log(e)
+
+})
