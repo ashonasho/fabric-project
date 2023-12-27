ade6345787756dc6b232f3c6bf86aabebad584a2
ashonasho <ashonmcmillan1234@gmail.com>
2023-12-27 14:57:08 +0530

    toggle pan is working

--- index.js
+++ index.js
@@ -23,8 +23,41 @@ const togglePan= () => {
 
 }
 
+const setPanEvents = (canvas) => {
+        // mouse:over
+    canvas.on('mouse:move', (event) => {
+        // console.log(event)
+        if (mousePressed && currentMode === modes.pan) {
+            canvas.setCursor('grab')
+            canvas.renderAll()
+            const mEvent = event.e;
+            const delta = new fabric.Point(mEvent.movementX,mEvent.movementY)
+            canvas.relativePan(delta)
+        } 
+    }) 
+
+    // mouse:down
+    canvas.on("mouse:down", (event) => {
+        mousePressed = true;
+        if (currentMode === modes.pan) {
+            canvas.setCursor('grab')
+            canvas.renderAll()
+        }
+        
+    })
+
+    // mouse:up
+    canvas.on("mouse:up", (event) => {
+        mousePressed = false
+        canvas.setCursor('default')
+        canvas.renderAll()
+    })
+
+
+}
+
 const canvas = initCanvas("canvas");
-let mousePressed = false;
+let mousePressed = true;
 
 let currentMode;
 
@@ -35,32 +68,4 @@ const modes = {
 
 setBackground('https://i.pinimg.com/564x/aa/85/fe/aa85fedb590860fdeff1731fbb52ddb0.jpg', canvas);
 
-// mouse:over
-canvas.on('mouse:move', (event) => {
-    // console.log(event)
-    if (mousePressed && currentMode === modes.pan) {
-        canvas.setCursor('grab')
-        canvas.renderAll()
-        const mEvent = event.e;
-        const delta = new fabric.Point(mEvent.movementX,mEvent.movementY)
-        canvas.relativePan(delta)
-    } 
-}) 
-
-// mouse:down
-canvas.on("mouse:down", (event) => {
-    mousePressed = true;
-    if (currentMode === modes.pan) {
-        canvas.setCursor('grab')
-        canvas.renderAll()
-    }
-    
-})
-
-// mouse:up
-canvas.on("mouse:up", (event) => {
-    mousePressed = false
-    canvas.setCursor('default')
-    canvas.renderAll()
-})
-
+setPanEvents(canvas)
